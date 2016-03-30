064cd16bac1a23f0d15320fd017da89e37abdc98
Vadim Mazur <mazurva@gmail.com>
2016-03-30 18:35:12 +0600

    Update UpdateAction.php

--- actions/UpdateAction.php
+++ actions/UpdateAction.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace sibds\controllers\actions;
+
 use creocoder\nestedsets\NestedSetsBehavior;
 
 /**
@@ -16,20 +17,33 @@ class UpdateAction extends BaseAction
         $model = $this->getModel();
 
         if ($model->load(\Yii::$app->request->post()) && $model->validate()) {
-            if(($behavior = $this->testBehavior(new NestedSetsBehavior())) !== false){
-                if($model->hasAttribute($behavior->treeAttribute)){
-                    $model->makeRoot();
-                }else{
-                    $modelName = $this->getModelName;
-                    if($modelName::find()->roots()->count()<=0){
-                        $root=new $modelName(['name'=>'Основная']);
+            if (($behavior = $this->testBehavior(new NestedSetsBehavior())) !== false) {
+                if ($model->hasAttribute($behavior->treeAttribute)) {
+                    $modelName = $this->getModelName();
+
+                    if (($parent = \Yii::$app->request->get('parent')) === null) {
+                        $model->makeRoot();
+                    } else if (($root = $modelName::findOne(['id' => $parent])) === null)
+                        throw new NotFoundHttpException('The specified record cannot be found.');
+
+                    if ($model->isNewRecord) {
+                        $model->appendTo($root);
+                    }
+                } else {
+                    $modelName = $this->getModelName();
+                    if ($modelName::find()->roots()->count() <= 0) {
+                        $root = new $modelName(['name' => 'Основная']);
                         $root->makeRoot();
                     }
+                    if (($parent = \Yii::$app->request->get('parent')) === null)
+                        $root = $modelName::find()->roots()->one();
+                    else if (($root = $modelName::findOne(['id' => $parent])) === null)
+                        throw new NotFoundHttpException('The specified record cannot be found.');
+
                     $root = $modelName::find()->roots()->one();
                     $model->appendTo($root);
                 }
-            }
-            else
+            } else
                 $model->save();
 
             if (isset($_GET['close']))
